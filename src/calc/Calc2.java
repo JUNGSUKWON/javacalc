d0156f8047094c2ced18546773b4a60d19962411
SAMSUNG <SAMSUNG@LAPTOP-J5U22BL3>
2021-11-24 11:31:48 +0900

    계산기 사칙연산 체크넘버

--- src/calc/Calc2.java
+++ src/calc/Calc2.java
@@ -26,21 +26,67 @@ public class Calc2 {
 //		System.out.print("입력:");
 		this.op = sc.next();
 	}
+	//숫자를 체크 기능
+	public boolean checkNumber() {
+		
+		//리턴값
+		boolean isCheck = false;
+		//빼기할 때 체크(첫번째 수가 두번째 수보다 커야)
+		if(this.op.equals("-")) {
+			if(this.num1<this.num2) {
+				System.out.println("첫번째 수가 두번째 수보다 커야합니다! 다시 입력해주세요!");
+			}else {
+				isCheck=true;
+			}
+		}
+		//나누기 할때(첫번째 수가 두번째 수보다 커야한다.나누는 숫자가 0이 아니어야 한다)
+		else if(this.op.equals("/")) {
+			if(this.num1<this.num2) {//두번째숫자가 0일때 더 클수가없다.
+				System.out.println("첫번쨰 수가 두번째 수보다 커야합니다! 다시 입력해주세요!");
+				
+			}else if(this.num2==0) {
+				System.out.println("두번째 수가 0이여서는 안되요! 다시 입력해주세요!" );
+
+			}else {
+				isCheck=true;
+			}
+		}else {
+			isCheck = true;
+		}
+		return isCheck;
+	}
+	
 	//결과를 출력해주는 기능
 	public void printResult() {
 		if(this.op.equals("+")) {
 			System.out.println(this.num1+" "+this.op+" "+this.num2+" = "+plus());
-			
+		}else if(this.op.equals("-")) {
+			System.out.println(this.num1+" "+this.op+" "+this.num2+" = "+minus());
+		}else if(this.op.equals("*")) {
+			System.out.println(this.num1+" "+this.op+" "+this.num2+" = "+mul());
+		}else if(this.op.equals("/")) {
+			System.out.println(this.num1+" "+this.op+" "+this.num2+" = "+div());
+		}else {
+			System.out.println("잘못된 연산자");
 		}
-		
 	}
-	
+
 	//더하기
 	public int plus() {
 		return this.num1+this.num2;
 	}
 	//빼기
+	public int minus() {
+		return this.num1-this.num2;
+	}
 	//곱하기
+	public int mul() {
+		
+		return this.num1*this.num2;
+	}
 	//나누기
+	public int div() {
+		return this.num1/this.num2;
+	}
 
 }
--- src/calc/MainClass.java
+++ src/calc/MainClass.java
@@ -3,22 +3,27 @@ package calc;
 import com.jsw.util.Util;
 
 public class MainClass {
-	
+
 	public static void main(String[] args) {
-			
+
 		System.out.println("계산기 프로그램");
 		//계산기 클래스를 선언(객체생성,인스턴스 생성)
 		Calc2 cc2 = new Calc2();
-		//두개의 숫자를 입력하시오
-		System.out.println("두개의 숫자를 입력하시오");
-		cc2.inputNumber();
-		//연산자를 입력하시오
-		System.out.print("연산자를 입력하시오:");
-		cc2.inputOper();
-		
+
+		do {
+			//두개의 숫자를 입력하시오
+			System.out.println("두개의 숫자를 입력하시오");
+			cc2.inputNumber();
+			//연산자를 입력하시오
+			System.out.print("연산자를 입력하시오:");
+			cc2.inputOper();
+		}while(!cc2.checkNumber());
+
+
+		//결과
 		cc2.printResult();
-		
-		
-		
+
+
+
 	}
 }
